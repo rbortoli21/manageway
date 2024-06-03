ff10fa81cc76b31d8062485dff23a451010cb3b9
Rafael <rafaelbortoli21@gmail.com>
2024-06-03 14:38:43 -0300

    initial files

---
+++ src/main/kotlin/com/manageway/ManagewayApplication.kt
@@ -0,0 +1,11 @@
+package com.manageway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManagewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<ManagewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/manageway/ManagewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.manageway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManagewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
